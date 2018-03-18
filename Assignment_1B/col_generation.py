9f374840f18e44e738904d0e578f95fea6db2ec1
OWM <Olivier.thijssens@gmail.com>
2018-03-18 19:09:48 +0100

    col+row added

--- Assignment_1B/col_generation.py
+++ Assignment_1B/col_generation.py
@@ -1,20 +1,4 @@
-import pandas as pd
-import numpy as np
-import copy
-
-## Load data
-#xl = pd.ExcelFile("Input_AE4424_Ass1P2.xlsx")
-#dfs = {sheet: xl.parse(sheet) for sheet in xl.sheet_names}
-#dfs['Flight'] = dfs['Flight'].set_index('Flight Number')
-#flights = dfs["Flight"].index.values.tolist()
-
-#itin = dfs["Itinerary"]["Itin No."].tolist()
-#A_1 = np.zeros((len(flights), len(itin)))
-#p_index = [1]*len(A_1[0])
-#sig = [0]*len(dfs['Itinerary'].index.values)
-#DV_label_list = []
-
-def col_generation(RMP,dfs, pi,sig, p_index_list, DV_label_list):
+def col_generation(RMP,dfs, pi,sig_vect, p_index_list):
     ## Create sets
     flights = dfs["Flight"].index.values.tolist()
     recapture = dfs["Recapture Rate"].index.values
@@ -47,10 +31,10 @@ def col_generation(RMP,dfs, pi,sig, p_index_list, DV_label_list):
         r_total_pi = sum(r_pi_list)
 
 
-        tpr = fare_p - bpr * fare_r - p_total_pi + bpr * r_total_pi - sig[p]
-        if tpr < 0:
+        tpr = fare_p - bpr * fare_r - p_total_pi + bpr * r_total_pi - sig_vect[p]
+        if round(tpr,5) < 0:
+            col_added = True
             cost = fare_p - bpr * fare_r
-
             p_leg_ind_list = []
             for leg in p_flight_numbers:
                 p_index = flights.index(leg)
@@ -64,69 +48,16 @@ def col_generation(RMP,dfs, pi,sig, p_index_list, DV_label_list):
             same_flight_index = None
             for i in range(len(p_leg_ind_list)):
                 if p_leg_ind_list[i] in r_leg_ind_list:
-                    print(p_leg_ind_list)
-                    print(r_leg_ind_list)
                     same_flight_index = p_leg_ind_list[i]
                     p_leg_ind_list = list(set(p_leg_ind_list)-{p_leg_ind_list[i]})
                     r_leg_ind_list = list(set(r_leg_ind_list) - {r_leg_ind_list[i]})
                     break
-            print([[p_leg_ind_list + r_leg_ind_list],
-                [1] * len(p_leg_ind_list) + [-bpr] * len(r_leg_ind_list)])
-            print(p)
-            print(r)
-            DV_label_list.append('t' + '_' + str(p) + '_' + str(r))
             p_index_list.append(p)
             if same_flight_index is not None:
-                RMP.variables.add(obj=[cost],names=[DV_label_list[-1]],columns=[[p_leg_ind_list+r_leg_ind_list + [same_flight_index],
+                RMP.variables.add(obj=[cost],names=['t_'+str(p)+'_'+str(r)],columns=[[p_leg_ind_list+r_leg_ind_list + [same_flight_index],
                                                                              [1]*len(p_leg_ind_list) + [-bpr]*len(r_leg_ind_list) + [1 - bpr]]])
             else:
-                RMP.variables.add(obj=[cost], names=[DV_label_list[-1]], columns=[[p_leg_ind_list + r_leg_ind_list,
+                RMP.variables.add(obj=[cost], names=['t_'+str(p)+'_'+str(r)], columns=[[p_leg_ind_list + r_leg_ind_list,
                 [1] * len(p_leg_ind_list) + [-bpr] * len(r_leg_ind_list)]])
 
-
-    return(RMP,p_index_list,DV_label_list)
-
-
-# def col_generation(model, original_graph, pi, sig, k, A_ineq, A_eq, com_added=None):
-#     # adjust graph with new costs
-#     if com_added is None:
-#         com_added = []
-#     graph=copy.deepcopy(original_graph)
-#     C_real=float(0)
-#     for i in range(len(arcs)):
-#         origin = dfs['Arcs'].From[i]
-#         destination = dfs['Arcs'].To[i]
-#         graph[origin][destination] = original_graph[origin][destination] - pi[i]
-#     for c in range(len(commodities)):
-#         path_new, C_new = dijkstra(graph, dfs['Commodities'].From[c], dfs['Commodities'].To[c])
-#         if C_new < sig[c] / quantity[c]:
-#             A_ineq_add = np.zeros([len(arcs),1])
-#             A_eq_add = np.zeros([len(commodities),1])
-#             A_eq_add[c] = 1
-#             com_added.append(c+1)
-#             for j in range(len(path_new) - 1):
-#                 index = dfs['Arcs'].index[(dfs['Arcs'].From == path_new[j]) & (dfs['Arcs'].To == path_new[j + 1])]
-#                 A_ineq_add[index] = 1 * quantity[c]
-#                 C_real = float(C_real+dfs['Arcs'].Cost[index])
-#             A_ineq = np.hstack((A_ineq, A_ineq_add))
-#             A_eq = np.hstack((A_eq, A_eq_add))
-#             model.variables.add(obj=[C_real*quantity[c]],
-#                                 names=['f_k' + str(c+1) + '_' + str(k)])
-#     model.linear_constraints.delete()
-#     # Add ineq constraints
-#     constraints_ineq = list()
-#     for i in range(len(arcs)):
-#         constraints_ineq.append([A_ineq[i, :].nonzero()[0].tolist(), A_ineq[i, A_ineq[i, :].nonzero()[0]].tolist()])
-#     model.linear_constraints.add(
-#         lin_expr=constraints_ineq,
-#         senses=['L'] * len(arcs),
-#         rhs=rhs_ineq)
-#     # Add eq constraints
-#     constraints_eq = list()
-#     for i in range(len(commodities)):
-#         constraints_eq.append([A_eq[i, :].nonzero()[0].tolist(), A_eq[i, A_eq[i, :].nonzero()[0]].tolist()])
-#     model.linear_constraints.add(
-#         lin_expr=constraints_eq,
-#         senses=['E'] * len(commodities),
-#         rhs=rhs_eq)
-#     return model, A_eq, A_ineq, com_added
+    return(RMP,p_index_list, col_added)
--- Assignment_1B/question1.py
+++ Assignment_1B/question1.py
@@ -1,12 +1,75 @@
 import cplex
-import copy
 import numpy as np
 import pandas as pd
-from col_generation import col_generation
+# from col_generation import col_generation
 
+def col_generation(RMP,dfs, pi,sig_vect, p_index_list):
+    ## Create sets
+    flights = dfs["Flight"].index.values.tolist()
+    recapture = dfs["Recapture Rate"].index.values
+    plist = dfs['Recapture Rate']['From Itinerary'].tolist()
+    rlist = dfs['Recapture Rate']['To Itinerary'].tolist()
+    fareplist = dfs['Recapture Rate']["Fare 'From'"].tolist()
+    farerlist = dfs['Recapture Rate']["Fare 'To' "].tolist()
+    bprlist = dfs['Recapture Rate']["Recapture Rate"].tolist()
+    col_added = False
+    for i in recapture:
+        p = plist[i]
+        r = rlist[i]
+        fare_p = fareplist[i]
+        fare_r = farerlist[i]
+        bpr = bprlist[i]
+        #find flights belonging to p
+        p_flight_numbers = dfs['Itinerary'].loc[ p , 'Leg 1' : 'Leg 2'].tolist()
+        p_flight_numbers = [x for x in p_flight_numbers if str(x) != 'nan']
+        p_pi_list = []
+        for j, flight_number in enumerate(p_flight_numbers):
+            p_pi_list.append(pi[flights.index(flight_number)])
+        p_total_pi = sum(p_pi_list)
+
+        # find flights belonging to r
+        r_flight_numbers = dfs['Itinerary'].loc[r, 'Leg 1': 'Leg 2'].tolist()
+        r_flight_numbers = [x for x in r_flight_numbers if str(x) != 'nan']
+        r_pi_list = []
+        for j, flight_number in enumerate(r_flight_numbers):
+            r_pi_list.append(pi[flights.index(flight_number)])
+        r_total_pi = sum(r_pi_list)
+
+
+        tpr = fare_p - bpr * fare_r - p_total_pi + bpr * r_total_pi - sig_vect[p]
+        print(tpr)
+        if round(tpr,5) < 0:
+            col_added = True
+            cost = fare_p - bpr * fare_r
+            p_leg_ind_list = []
+            for leg in p_flight_numbers:
+                p_index = flights.index(leg)
+                p_leg_ind_list.append(p_index)
+
+            r_leg_ind_list = []
+            for leg in r_flight_numbers:
+                r_index = flights.index(leg)
+                r_leg_ind_list.append(r_index)
+
+            same_flight_index = None
+            for i in range(len(p_leg_ind_list)):
+                if p_leg_ind_list[i] in r_leg_ind_list:
+                    same_flight_index = p_leg_ind_list[i]
+                    p_leg_ind_list = list(set(p_leg_ind_list)-{p_leg_ind_list[i]})
+                    r_leg_ind_list = list(set(r_leg_ind_list) - {r_leg_ind_list[i]})
+                    break
+            p_index_list.append(p)
+            if same_flight_index is not None:
+                RMP.variables.add(obj=[cost],lb=[0],names=['t_'+str(p)+'_'+str(r)],columns=[[p_leg_ind_list+r_leg_ind_list + [same_flight_index],
+                                                                             [1]*len(p_leg_ind_list) + [-bpr]*len(r_leg_ind_list) + [1 - bpr]]])
+            else:
+                RMP.variables.add(obj=[cost], lb=[0],names=['t_'+str(p)+'_'+str(r)], columns=[[p_leg_ind_list + r_leg_ind_list,
+                [1] * len(p_leg_ind_list) + [-bpr] * len(r_leg_ind_list)]])
+
+    return(RMP,p_index_list, col_added)
 
 ## Load data
-xl = pd.ExcelFile("Input_AE4424_Ass1P2.xlsx")
+xl = pd.ExcelFile("Assignment_1B/Input_AE4424_Ass1P2.xlsx")
 dfs = {sheet: xl.parse(sheet) for sheet in xl.sheet_names}
 dfs['Flight'] = dfs['Flight'].set_index('Flight Number')
 
@@ -20,6 +83,7 @@ Demand = dfs["Itinerary"]["Demand"].tolist()
 RMP = cplex.Cplex()
 RMP.objective.set_sense(RMP.objective.sense.minimize)
 RMP.variables.add(obj=Fare,
+                  lb=[0]*len(itin),
                   names=['t' + str(p) + '_x' for p in range(len(itin))])
 
 ## Add constraint set 1
@@ -30,10 +94,10 @@ for i in range(len(flights)):
     leg2_ind = dfs["Itinerary"].index[dfs["Itinerary"]["Leg 2"] == flights[i]].tolist()
     index_it = leg1_ind + leg2_ind
     A_1[i, index_it] = 1
-    q = dfs['Flight'].loc[flights[i], 'Capacity']
-    cap1 = int(sum(dfs['Itinerary'].loc[dfs['Itinerary']['Leg 1'] == flights[i], 'Demand'].tolist()))
-    cap2 = int(sum(dfs['Itinerary'].loc[dfs['Itinerary']['Leg 2'] == flights[i], 'Demand'].tolist()))
-    rhs_1.append(float(q - cap1 - cap2))
+    cap = dfs['Flight'].loc[flights[i], 'Capacity']
+    dem1 = int(sum(dfs['Itinerary'].loc[dfs['Itinerary']['Leg 1'] == flights[i], 'Demand'].tolist()))
+    dem2 = int(sum(dfs['Itinerary'].loc[dfs['Itinerary']['Leg 2'] == flights[i], 'Demand'].tolist()))
+    rhs_1.append(float(dem1 + dem2 - cap))
     RMP.linear_constraints.add(
         lin_expr=[[index_it, A_1[i, index_it].tolist()]],
         senses=['G'],
@@ -50,78 +114,57 @@ sol_names = np.array(RMP.variables.get_names())
 obj = RMP.solution.get_objective_value()
 
 pi = np.array(RMP.solution.get_dual_values()[:len(flights)])
-sig = np.array(RMP.solution.get_dual_values()[len(flights):])
+sig_it_index_list = []  # list of indices of itineraries for which row constraint is added
+sig_vect = np.array([0]*len(dfs['Itinerary']))  # initially we do not have sigma -> set to 0 for pricing problem
 RMP.write('rmp.lp')
 
-p_index = [1]*len(A_1[0])
-sig = [0]*len(dfs['Itinerary'].index.values)
-DV_label_list = []
 
-RMP,p_index_list,DV_label_list = col_generation(RMP, dfs, pi,sig, p_index, DV_label_list)
+##  COLUMN & ROW GENERATION
+Opt_Row = False
+Opt_Col = False
+p_index_list = np.arange(len(itin)).tolist()  # row of p-numbers for all variables added
+while Opt_Row is False or Opt_Col is False:
+    # Column Generation loop
+    while Opt_Col is False:
+        col_added = False
+        RMP, p_index_list, col_added = col_generation(RMP, dfs, pi, sig_vect, p_index_list)
+        if col_added is True:
+            RMP.solve()
+            print("Cost            : {0:.5f}".format(RMP.solution.get_objective_value()))
+            sol = np.array(RMP.solution.get_values())
+            sol_names = np.array(RMP.variables.get_names())
+            obj = RMP.solution.get_objective_value()
+            pi = np.array(RMP.solution.get_dual_values()[:len(flights)])
+            if len(np.array(RMP.solution.get_dual_values()[len(flights):])) != 0:
+                sig_vect[sig_it_index_list] = np.array(RMP.solution.get_dual_values()[len(flights):])
+            Opt_Row = False
+        else:  # No column added, so can move to row generation
+            Opt_Col = True
 
+    # Row Generation loop
+    while Opt_Row is False:
+        row_added = False
+        for p in range(len(itin)):
+            if sum(sol[np.array(p_index_list)==p]) > Demand[p]:
+                RMP.linear_constraints.add(
+                    lin_expr=[[list(sol_names[np.array(p_index_list)==p]), [1]*sum(np.array(p_index_list)==p)]],
+                    senses=['L'],
+                    rhs=[Demand[p]])
+                sig_it_index_list.append(p)
+                row_added = True
+        if row_added is True:
+            RMP.solve()
+            print("Cost            : {0:.5f}".format(RMP.solution.get_objective_value()))
+            print("Solution status :", RMP.solution.get_status())
+            sol = np.array(RMP.solution.get_values())
+            print(sol)
+            sol_names = np.array(RMP.variables.get_names())
+            obj = RMP.solution.get_objective_value()
+            pi = np.array(RMP.solution.get_dual_values()[:len(flights)])
+            sig_vect[sig_it_index_list] = np.array(RMP.solution.get_dual_values()[len(flights):])
+            Opt_Col = False
+        else:
+            Opt_Row = True
 
+RMP.write('rmp.lp')
 
-
-
-
-#
-# ## COLUMN GENERATION ------------------------------------------------------
-# original_graph = copy.deepcopy(graph)
-# commodity_order = list(range(1, len(commodities) + 1))
-# sig_vect = np.copy(sig)
-# quantity_vect = np.copy(quantity)
-# sig_vect[0] = 10000  # to get the while loop started
-# delta = np.copy(A_ineq[:, 0:len(commodities)])
-# delta[delta > 0] = 1
-# c_ij = dfs['Arcs']['Cost'].values
-# new_cost = np.array([c_ij - pi])
-#
-# k = 1
-# while (np.inner(new_cost, delta.transpose()) < sig_vect / quantity_vect).any():
-#     RMP, A_eq, A_ineq, com_added = col_generation(RMP, original_graph, pi, sig, k, A_ineq, A_eq)
-#     # solve model
-#     RMP.solve()
-#     print("Solution status :", RMP.solution.get_status())
-#     print("Cost            : {0:.5f}".format(RMP.solution.get_objective_value()))
-#     print()
-#     sol = RMP.solution.get_values()
-#     obj = RMP.solution.get_objective_value()
-#     pi = np.array(RMP.solution.get_dual_values()[:len(arcs)])
-#     sig = np.array(RMP.solution.get_dual_values()[len(arcs):])
-#
-#     delta = np.hstack((delta, A_ineq[:, -len(com_added):]))
-#     delta[delta > 0] = 1
-#     new_cost = np.array([c_ij - pi])
-#     commodity_order = commodity_order + list(com_added)
-#     sig_vect = sig[np.array(commodity_order) - 1]
-#     quantity_vect = np.append(quantity_vect, quantity[np.array(com_added) - 1])
-#     k += 1
-# RMP.write('rmp.lp')
-
-
-
-# p_index = np.arange(0,len(itin))
-# ##  Row generation
-# Opt_Row = False
-#
-# # solve separation problem
-# while Opt_Row is False:
-#     for p in range(len(itin)):
-#         if sum(sol[p_index==p]) <= Demand[p]:
-#             RMP.linear_constraints.add(
-#                 lin_expr=[[list(sol_names[p_index==p]), [1]*sum(p_index==p)]],
-#                 senses=['L'],
-#                 rhs=[Demand[p]])
-#             Opt_Col = False
-#             RMP.solve()
-#             print("Solution status :", RMP.solution.get_status())
-#             sol = np.array(RMP.solution.get_values())
-#             sol_names = np.array(RMP.variables.get_names())
-#             obj = RMP.solution.get_objective_value()
-#             pi = np.array(RMP.solution.get_dual_values()[:len(flights)])
-#             sig = np.array(RMP.solution.get_dual_values()[len(flights):])
-#         else:
-#             Opt_Row = True
-
-
-RMP.linear_constraints.add()
