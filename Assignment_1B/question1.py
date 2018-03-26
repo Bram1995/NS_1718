6d97696c7fd5bbe5875db2eca6357485e2b890d9
OWM <Olivier.thijssens@gmail.com>
2018-03-26 21:35:08 +0200

    1B

--- Assignment_1B/question1.py
+++ Assignment_1B/question1.py
@@ -1,6 +1,7 @@
 import cplex
 import numpy as np
 import pandas as pd
+import time
 # from col_generation import col_generation
 def col_generation(RMP,dfs, pi,sig_vect, p_index_list,vars_added):
     ## Create sets
@@ -59,16 +60,18 @@ def col_generation(RMP,dfs, pi,sig_vect, p_index_list,vars_added):
                     break
             p_index_list.append(p)
             if same_flight_index is not None:
-                RMP.variables.add(obj=[cost],lb=[0],names=['t_'+str(p)+'_'+str(r)],columns=[[p_leg_ind_list+r_leg_ind_list + [same_flight_index],
+                RMP.variables.add(obj=[cost],lb=[0],names=['t'+str(p)+'_'+str(r)+'.'],columns=[[p_leg_ind_list+r_leg_ind_list + [same_flight_index],
                                                                              [1]*len(p_leg_ind_list) + [-bpr]*len(r_leg_ind_list) + [1 - bpr]]])
             else:
-                RMP.variables.add(obj=[cost], lb=[0],names=['t_'+str(p)+'_'+str(r)], columns=[[p_leg_ind_list + r_leg_ind_list,
+                RMP.variables.add(obj=[cost], lb=[0],names=['t'+str(p)+'_'+str(r)+'.'], columns=[[p_leg_ind_list + r_leg_ind_list,
                 [1] * len(p_leg_ind_list) + [-bpr] * len(r_leg_ind_list)]])
 
     return(RMP,p_index_list, col_added,vars_added)
 
+start_time = time.time()
+
 ## Load data
-xl = pd.ExcelFile("Input_AE4424_Ass1P2.xlsx")
+xl = pd.ExcelFile("Assignment_1B/Input_AE4424_Ass1P2.xlsx")
 dfs = {sheet: xl.parse(sheet) for sheet in xl.sheet_names}
 dfs['Flight'] = dfs['Flight'].set_index('Flight Number')
 
@@ -116,12 +119,15 @@ pi = np.array(RMP.solution.get_dual_values())
 sig_it_index_list = []  # list of indices of itineraries for which row constraint is added
 vars_added = []  # list of indices of recapture variables already added to model by column generation
 sig_vect = np.array([0]*len(dfs['Itinerary']))  # initially we do not have sigma -> set to 0 for pricing problem
-RMP.write('rmp.lp')
 
 
 ##  COLUMN & ROW GENERATION
 Opt_Row = False
 Opt_Col = False
+itcount = 0  # counter for number of iterations of the outer while loop
+itcount_col = 0  # counter for number of iterations of the CG while loop
+itcount_row = 0  # counter for number of iterations of the row gen while loop
+
 p_index_list = np.arange(len(itin)).tolist()  #i row of p-numbers for all variables added
 while Opt_Row is False or Opt_Col is False:
     # Column Generation loop
@@ -129,6 +135,8 @@ while Opt_Row is False or Opt_Col is False:
         col_added = False
         RMP, p_index_list, col_added, vars_added = col_generation(RMP, dfs, pi, sig_vect, p_index_list, vars_added)
         if col_added is True:
+            itcount_col += 1
+            print("Added columns")
             RMP.solve()
             print("Cost            : {0:.5f}".format(RMP.solution.get_objective_value()))
             sol = np.array(RMP.solution.get_values())
@@ -153,6 +161,8 @@ while Opt_Row is False or Opt_Col is False:
                 sig_it_index_list.append(p)
                 row_added = True
         if row_added is True:
+            itcount_row += 1
+            print("Added rows")
             RMP.solve()
             print("Cost            : {0:.5f}".format(RMP.solution.get_objective_value()))
             print("Solution status :", RMP.solution.get_status())
@@ -164,12 +174,30 @@ while Opt_Row is False or Opt_Col is False:
             Opt_Col = False
         else:
             Opt_Row = True
+    itcount += 1
+
+print("Total computational time is " + str(time.time() - start_time) + " seconds")
+print("Total number of columns added is " + str(len(vars_added)))
+print("Total number of rows added is " + str(RMP.linear_constraints.get_num() - len(flights)))
+print("Total number of iterations done is " + str(itcount_col))
+print("Total number of iterations through column while loop is " + str(itcount_col))
+print("Total number of iterations through row while loop is " + str(itcount_row))
 
 RMP.write('rmp.lp')
 
 
-##  Get flow per flightleg between hubs and division of itnierary pax
+##  Get flow per flightleg between hubs and division of itinerary pax
 df_hubflights = dfs['Flight'].loc[dfs['Flight']["ORG"].isin(["AEP","EZE"]) & dfs['Flight']["DEST"].isin(["AEP","EZE"])]
-for i in iterrows(df_hubflights):
-    flow[i] = Q_i - sum_r t_ir + sum_r b_ri t_ri
-
+df_flow = pd.DataFrame({"Flight(leg)"},{"Itinerary"},{"Flow"})
+for i,row in df_hubflights.iterrows():
+    leg1_ind = dfs["Itinerary"].loc[dfs["Itinerary"]["Leg 1"] == i]
+    leg2_ind = dfs["Itinerary"].loc[dfs["Itinerary"]["Leg 2"] == i]
+    index_it = pd.concat([leg1_ind,leg2_ind])
+    for j,rowj in index_it.iterrows():
+        ind_spill = [i for i, x in enumerate(sol_names) if ("t%s_" %j) in x]
+        ind_recap = [i for i, x in enumerate(sol_names) if ("_%s." %j) in x]
+        spilled_pax = np.sum(sol[ind_spill])
+        recap_pax = np.sum(sol[ind_recap])
+        flow = rowj["Demand"] - spilled_pax + recap_pax
+        df_flow
+        print(i,j,flow, rowj["Demand"])
