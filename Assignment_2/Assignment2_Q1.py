5013250cdfcd2b5fe4b801e1040950b11ce2fd89
Bram1995 <37330453+Bram1995@users.noreply.github.com>
2018-04-06 13:36:06 +0200

    woegf\

--- Assignment_2/Assignment2_Q1.py
+++ Assignment_2/Assignment2_Q1.py
@@ -17,28 +17,32 @@ flight_dfs = dfs['Flight'].set_index(['Flight Number'])
 flight_dfs.at[:,'Bus'] = bigM
 indices = flight_dfs.loc[(flight_dfs['ORG'] == 'AEP') & (flight_dfs['DEST'] == 'EZE') |
                          (flight_dfs['ORG'] == 'EZE') & (flight_dfs['DEST'] == 'AEP')].index.tolist()
-flight_dfs.at[indices, actype_dfs.index.values.tolist()] = 0
+flight_dfs.at[indices, actype_dfs.index.values.tolist()] = bigM
 flight_dfs.at[indices, actype_dfs.index.values.tolist()[-1]] = 4500
 flight_dfs = flight_dfs.fillna(bigM)
 airport_list = sorted(list(set(flight_dfs['ORG'])))
 
-
-
 nodes_dfs_dict = {}
+action_dict = {}
 for i in actype_dfs.index[:3]:
     tat = int(actype_dfs.loc[i,'TAT (min)'])
     flight_dataframe = copy.deepcopy(flight_dfs)
     flight_dataframe = flight_dataframe.loc[(flight_dataframe[i] != bigM),['ORG','DEST','Departure','Arrival',i]]
     flight_dataframe = flight_dataframe.loc[(flight_dataframe[i] != 0.),['ORG','DEST','Departure','Arrival',i]]
     flight_dataframe_sub_1 = flight_dataframe.loc[:,['ORG','Departure']].rename(columns ={'ORG':'Airport','Departure':'Time'})
+    flight_dataframe_sub_1.at[:,'Action'] = 'DEP'
     flight_dataframe_sub_2 = flight_dataframe.loc[:, ['DEST', 'Arrival']].rename(columns={'DEST': 'Airport', 'Arrival': 'Time'})
+    flight_dataframe_sub_2.at[:,'Action'] = 'ARR'
     for k in flight_dataframe_sub_2.index:
         time = datetime.combine(date(1,1,1),flight_dataframe_sub_2.loc[k,'Time'])
         flight_dataframe_sub_2.at[k,'Time'] = (time + timedelta(minutes= tat)).time()
     nodes_dataframe = copy.deepcopy(pd.concat([flight_dataframe_sub_1,flight_dataframe_sub_2])).sort_values(['Airport','Time'],ascending=[True,True])
-    nodes_dataframe = nodes_dataframe.drop_duplicates(subset=['Airport','Time'])
-    nodes_dfs_dict[i]= copy.deepcopy(nodes_dataframe)
+    nodes_dataframe_action = copy.deepcopy(nodes_dataframe)
 
+    nodes_dataframe = nodes_dataframe.drop_duplicates(subset=['Airport','Time'])
+    nodes_dataframe = nodes_dataframe.drop('Action')
+    nodes_dfs_dict[i]= copy.deepcopy(nodes_dataframe)
+    action_dict[i] = copy.deepcopy(nodes_dataframe_action)
 ga_dict = {}
 ga_list = []
 for k in actype_dfs.index[:3]:
@@ -57,6 +61,3 @@ for k in actype_dfs.index[:3]:
                 ground_arc_dfs.at[index, ['Airport', 'Time1', 'Time2']] = [i,time1, time2]
             ga_list.append(copy.deepcopy(index))
         ga_dict[k + '_' + i] = copy.deepcopy(ground_arc_dfs)
-
-
-
